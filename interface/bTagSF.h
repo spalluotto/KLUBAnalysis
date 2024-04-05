9b47cdf7f502deac57d2b1b24ab4d620d34a39ee
Simona Palluotto <simona.palluotto@cern.ch>
2024-04-05 16:49:14 +0200

    fixing submitter

--- interface/bTagSF.h
+++ interface/bTagSF.h
@@ -13,10 +13,10 @@
 
 class bTagSF
 {
-public:
+ public:
   enum WP {loose = 0, medium = 1, tight = 2, reshaping = 3};
   enum SFsyst {central = 0, up = 1, down = 2};
-  bTagSF(std::string SFfilename, std::string effFileName, std::string effHistoTag, std::string year, std::string WPset);
+  bTagSF(std::string SFfilename, std::string effFileName, std::string effHistoTag, std::string year, std::string WPset, bool isMC);
   ~bTagSF();
   float getSF (WP wpt, SFsyst syst, int jetFlavor, float pt, float eta, float discr=0.);
   float getSFshifted (std::string systName, int jetFlavor, float pt, float eta, float discr=0.);
@@ -27,10 +27,10 @@ public:
   std::vector<float> getEvtWeight (const std::vector<std::pair<float, int>>& jets_and_btag, bigTree &theBigTree, std::map<int,double> jets_and_smearFactor, int channel, SFsyst systWP);
   std::vector<float> getEvtWeightShifted (const std::vector<std::pair<float,int>>& jets_and_btag, bigTree &theBigTree, std::map<int,double> jets_and_smearFactor);
 
-private:
+ private:
   static constexpr int m_nWP = 3;
   void m_initialize(std::string, std::string, std::string, std::string, std::string);
-	
+  
   // related to scale factors
   BTagCalibration m_calib;
   BTagCalibrationReader m_readers [m_nWP+1]; // [loose, medium, tight, reshaping]
