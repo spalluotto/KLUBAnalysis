98fb1a87b3c9f46a87534f8f2590c5134ad9974e
Simona Palluotto <simona.palluotto@cern.ch>
2024-05-23 17:45:07 +0200

    config updated

--- src/AnalysisHelper.cc
+++ src/AnalysisHelper.cc
@@ -224,19 +224,14 @@ std::shared_ptr<Sample> AnalysisHelper::openSample(std::string sampleName)
 						   sampleCfg_->readStringOpt(Form("samples::%s", sampleName.c_str())) );
   std::vector<std::string> filenames = {{
 	  filename + std::string("/goodfiles.txt"),
-	  filename + std::string("/goodfiles_resub1.txt"), filename + std::string("/goodfiles_resub2.txt"),
-	  filename + std::string("/goodfiles_resub3.txt"), filename + std::string("/goodfiles_resub4.txt"),
-	  filename + std::string("/goodfiles_resub5.txt"), filename + std::string("/goodfiles_resub6.txt"),
-	  filename + std::string("/goodfiles_resub7.txt"), filename + std::string("/goodfiles_resub8.txt"),
-	  filename + std::string("/goodfiles_resub9.txt")
+	  //filename + std::string("/goodfiles_resub1.txt"), filename + std::string("/goodfiles_resub2.txt"),
+	  //filename + std::string("/goodfiles_resub3.txt"), filename + std::string("/goodfiles_resub4.txt"),
+	  //filename + std::string("/goodfiles_resub5.txt"), filename + std::string("/goodfiles_resub6.txt"),
+	  //filename + std::string("/goodfiles_resub7.txt"), filename + std::string("/goodfiles_resub8.txt"),
+	  //filename + std::string("/goodfiles_resub9.txt")
 	}};
 
-  std::shared_ptr<Sample> sample(new Sample(sampleName, filenames,
-											"HTauTauTree",
-											sampleCfg_->readStringOpt(Form("evaluation::tree")),
-											sampleCfg_->readStringOpt(Form("samples::base")),
-											sampleCfg_->readStringOpt(Form("evaluation::base")),
-											"h_eff", 1));
+  std::shared_ptr<Sample> sample(new Sample(sampleName, filenames, "HTauTauTree", sampleCfg_->readStringOpt(Form("evaluation::tree")), sampleCfg_->readStringOpt(Form("samples::base")), sampleCfg_->readStringOpt(Form("evaluation::base")), "h_eff", 1));
 
   if (sampleCfg_->hasOpt(Form("userEffBin::%s", sampleName.c_str())))    
     {
